8059a18b86c8f91ef27cb063229e1a11ce30a5ea
DavidTufi120 <davizinhol_@Hotmail.com>
2025-05-22 17:37:20 -0300

    updated streght visual

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -19,32 +19,39 @@ export class SignupComponent implements OnInit {
     passwordError = '';
     existingUser: boolean = false;
 
+    // Flags para cada critério
+    hasMinLength = false;
+    hasUpper = false;
+    hasLower = false;
+    hasSpecial = false;
+    noRepeatedNumbers = true;
+
     constructor(private router: Router, private http: HttpClient) { }
 
     ngOnInit(): void { }
 
     validatePassword(password: string) {
-        const minLength = /.{8,}/;
-        const upperCharacters = /[A-Z]/;
-        const lowerCharacters = /[a-z]/;
-        const specialCharacters = /[^A-Za-z0-9]/;
-        const repeatedNumbers = /(\d)\1{1,}/;
+        this.hasMinLength = password.length >= 8;
+        this.hasUpper = /[A-Z]/.test(password);
+        this.hasLower = /[a-z]/.test(password);
+        this.hasSpecial = /[^A-Za-z0-9]/.test(password);
+        this.noRepeatedNumbers = !(/(\d)\1{1,}/.test(password));
 
         let errors = [];
-        if (!minLength.test(password)) errors.push('Mínimo 8 caracteres');
-        if (!upperCharacters.test(password)) errors.push('Pelo menos 1 letra maiúscula');
-        if (!lowerCharacters.test(password)) errors.push('Pelo menos 1 letra minúscula');
-        if (!specialCharacters.test(password)) errors.push('Pelo menos 1 caractere especial');
-        if (repeatedNumbers.test(password)) errors.push('Não pode repetir números');
+        if (!this.hasMinLength) errors.push('Mínimo 8 caracteres');
+        if (!this.hasUpper) errors.push('Pelo menos 1 letra maiúscula');
+        if (!this.hasLower) errors.push('Pelo menos 1 letra minúscula');
+        if (!this.hasSpecial) errors.push('Pelo menos 1 caractere especial');
+        if (!this.noRepeatedNumbers) errors.push('Não pode repetir números');
 
         this.passwordError = errors.join(', ');
 
         let score = 0;
-        if (minLength.test(password)) score++;
-        if (upperCharacters.test(password)) score++;
-        if (lowerCharacters.test(password)) score++;
-        if (specialCharacters.test(password)) score++;
-        if (!repeatedNumbers.test(password)) score++;
+        if (this.hasMinLength) score++;
+        if (this.hasUpper) score++;
+        if (this.hasLower) score++;
+        if (this.hasSpecial) score++;
+        if (this.noRepeatedNumbers) score++;
 
         if (score <= 2) this.passwordStrength = 'Fraca';
         else if (score === 3 || score === 4) this.passwordStrength = 'Média';
