51e48392adbcbcacc278bac3a808cede965530d7
DavidTufi120 <davizinhol_@Hotmail.com>
2025-05-22 17:30:02 -0300

    implemented streght password to signup

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -15,13 +15,41 @@ import { environment } from '../../environments/environment';
 export class SignupComponent implements OnInit {
     email: string = '';
     password: string = '';
+    passwordStrength = '';
+    passwordError = '';
     existingUser: boolean = false;
 
     constructor(private router: Router, private http: HttpClient) { }
 
     ngOnInit(): void { }
 
+    validatePassword(password: string) {
+        const minLength = /.{8,}/;
+        const upperCharacters = /[A-Z]/;
+        const lowerCharacters = /[a-z]/;
+        const specialCharacters = /[^A-Za-z0-9]/;
+        const repeatedNumbers = /(\d)\1{1,}/;
 
+        let errors = [];
+        if (!minLength.test(password)) errors.push('Mínimo 8 caracteres');
+        if (!upperCharacters.test(password)) errors.push('Pelo menos 1 letra maiúscula');
+        if (!lowerCharacters.test(password)) errors.push('Pelo menos 1 letra minúscula');
+        if (!specialCharacters.test(password)) errors.push('Pelo menos 1 caractere especial');
+        if (repeatedNumbers.test(password)) errors.push('Não pode repetir números');
+
+        this.passwordError = errors.join(', ');
+
+        let score = 0;
+        if (minLength.test(password)) score++;
+        if (upperCharacters.test(password)) score++;
+        if (lowerCharacters.test(password)) score++;
+        if (specialCharacters.test(password)) score++;
+        if (!repeatedNumbers.test(password)) score++;
+
+        if (score <= 2) this.passwordStrength = 'Fraca';
+        else if (score === 3 || score === 4) this.passwordStrength = 'Média';
+        else this.passwordStrength = 'Forte';
+    }
 
     Signup() {
         this.http.post<any>(`${environment.apiUrl}/api/users`, {
