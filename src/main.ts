4488567237a89ed5ac778770fd89b5cb70088a73
DavidTufi120 <davizinhol_@Hotmail.com>
2025-05-23 17:40:21 -0300

    updated mains.ts jwt

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,15 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { provideRouter } from '@angular/router';
-import { provideAuth0 } from '@auth0/auth0-angular';
-import { environment } from './environments/environment';
 import { AppComponent } from './app/app.component';
 import { routes } from './app/app.routes';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { tokenInterceptor } from './app/token-interceptor/token.interceptor';
 
 bootstrapApplication(AppComponent, {
   providers: [
     provideRouter(routes),
-    provideHttpClient(),
-    provideAuth0({
-      domain: environment.auth0.domain,
-      clientId: environment.auth0.clientId,
-      authorizationParams: environment.auth0.authorizationParams
-    })
+    provideHttpClient(
+      withInterceptors([tokenInterceptor])
+    ),
   ]
 });
